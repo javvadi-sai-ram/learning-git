10a86fd526e0e3e24169a2cb398aa10901327a8d
javvadi-sai-ram <sairamjavvadi@gmail.com>
2022-07-12 19:15:42 +0530

    edited bannerCardList

--- src/App.js
+++ src/App.js
@@ -3,63 +3,63 @@ import RandomNumberGenerator from "./components/randomNumbergenerator"
 // import BannerCardItem from "./components/ipllogoboard/index.js";
 // import CardItem  from './components/technologyCards';
 
-// const bannerCardsList = [
-//   {
-//     id: 1,
-//     headerText: 'The Seasons Latest',
-//     description: 'Get the seasons all latest designs in a flick of your hand',
-//     className: 'card-1',
-//   },
-//   {
-//     id: 2,
-//     headerText: 'Our New Designs',
-//     description:
-//       'Get the designs developed by our in-house team all for yourself',
-//     className: 'card-2',
-//   },
-//   {
-//     id: 3,
-//     headerText: 'Insiders',
-//     description: 'Get the top class products for yourself with an extra off',
-//     className: 'card-3',
-//   },
-// ]
+const bannerCardsList = [
+  {
+    id: 1,
+    headerText: 'The Seasons Latest',
+    description: 'Get the seasons all latest designs in a flick of your hand',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    headerText: 'Our New Designs',
+    description:
+      'Get the designs developed by our in-house team all for yourself',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    headerText: 'Insiders',
+    description: 'Get the top class products for yourself with an extra off',
+    className: 'card-3',
+  },
+]
 
 
-// const cardsData = [
-//   {
-//     id: 1,
-//     title: 'Data Scientist',
-//     description:
-//       'Data scientists gather and analyze large sets of structured and unstructured data',
-//     imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
-//     className: 'card-1',
-//   },
-//   {
-//     id: 2,
-//     title: 'IOT Developer',
-//     description:
-//       'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
-//     imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
-//     className: 'card-2',
-//   },
-//   {
-//     id: 3,
-//     title: 'VR Developer',
-//     description:
-//       'A VR developer creates completely new digital environments that people can see.',
-//     imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
-//     className: 'card-3',
-//   },
-//   {
-//     id: 4,
-//     title: 'ML Engineer',
-//     description:
-//       'Machine learning engineers feed data into models defined by data scientists.',
-//     imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
-//     className: 'card-4',
-//   },
-// ]
+const cardsData = [
+  {
+    id: 1,
+    title: 'Data Scientist',
+    description:
+      'Data scientists gather and analyze large sets of structured and unstructured data',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    title: 'IOT Developer',
+    description:
+      'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    title: 'VR Developer',
+    description:
+      'A VR developer creates completely new digital environments that people can see.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
+    className: 'card-3',
+  },
+  {
+    id: 4,
+    title: 'ML Engineer',
+    description:
+      'Machine learning engineers feed data into models defined by data scientists.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
+    className: 'card-4',
+  },
+]
 
 
 
